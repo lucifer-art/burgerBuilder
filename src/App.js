aecbbd68b3c91afbbf120e261286c3a1b23b0e35
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-04-02 20:39:09 +0530

    added components and containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
+import Aux from './hoc/Aux';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Aux>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </Aux>
+    );
+  }
 }
 
 export default App;
