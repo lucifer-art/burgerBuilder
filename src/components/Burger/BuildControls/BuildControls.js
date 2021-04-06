41911e1ae856bfdab2b894fa325d57e505e17f22
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-04-06 19:55:07 +0530

    added order summary

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
+import { checkPropTypes } from 'prop-types';
 
 const controls = [
     {label:'Salad',type:'salad'},
@@ -9,12 +10,21 @@ const controls = [
     {label:'Meat',type:'meat'},
 ]
 
-const buildControls = () =>{
+const priceStyle = {
+    fontSize: '20px',
+    color:'green',
+}
+
+const buildControls = (props) =>{
     return (
         <div className={classes.BuildControls}>
+            <p>Current Price : <strong style={priceStyle}>$ {props.price.toFixed(2)}</strong></p>
             {controls.map(ctrl => {
-                return <BuildControl key={ctrl.label} label={ctrl.label} />
+                return <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} added={() => props.ingredientAdded(ctrl.type)} deducted={() => props.ingredientDeducted(ctrl.type)} disabled={props.disabled[ctrl.type]} />
             })}
+            <div>
+                <button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordering}>ORDER NOW</button>
+            </div>
         </div>
     ) 
 }
